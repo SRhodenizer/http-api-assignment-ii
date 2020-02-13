b58c0c94bb8854d1697cef763a1ca5bf51a7aa52
SRhodenizer <scr3876@g.rit.edu>
2020-02-12 23:51:24 -0500

    Buttons work kinda, data updates but pages change on button input.

---
+++ src/responses.js
@@ -0,0 +1,71 @@
+// data object
+const users = {};
+
+// responds with a json object
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+// responds without sending the json object
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+// returns user as json objects
+const getUsers = (request, response) => {
+  // makes a json object with the users data
+  const responseJSON = { users };
+  // responds with it
+  respondJSON(request, response, 200, responseJSON);
+};
+
+// adds a user to the users data
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
+
+  // if the needed params are not send tell the user
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    respondJSON(request, response, 400, responseJSON);
+    return;
+  }
+  // update default response code
+  let responseCode = 201;
+
+  // if it already exists update it
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    // if not add a new empty json obj
+    users[body.name] = {};
+  }
+
+  // update the new objects fields
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  // if the response code is still created return that message
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully.';
+    respondJSON(request, response, responseCode, responseJSON);
+    return;
+  }
+  // if not use the updated response code
+  respondJSONMeta(request, response, responseCode);
+};
+
+const getNotFound = (request, response) => {
+  respondJSON(request, response, 404, { id: 'notFound', message: 'The page you are looking for was not found.' });
+};
+
+// export methods
+module.exports = {
+  getUsers,
+  addUser,
+  getNotFound,
+};
---
+++ src/server.js
@@ -0,0 +1,82 @@
+// pulls in the http module
+const http = require('http');
+// get the url module
+const url = require('url');
+// querystring module for parsing querystrings in the url
+const query = require('querystring');
+// pull in the response finals
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./responses.js');
+
+// sets up the port for the server
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// handle GET requests
+const handleGet = (request, response, parsedUrl) => {
+  // route to correct method based on url
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else
+  if (parsedUrl.pathname === '/') {
+    htmlHandler.getIndex(request, response);
+  } else
+  if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response);
+  } else
+  if (parsedUrl.pathname === '/notReal') {
+    jsonHandler.getNotReal(request, response);
+  } else {
+    jsonHandler.getNotFound(request, response, 404);
+  }
+};
+
+const handlePost = (request, response, parsedUrl) => {
+    //if the post call is addUser 
+  if (parsedUrl.path === '/addUser') {
+      //make the response accessible 
+    const res = response;
+      
+      //make a body array 
+    const body = [];
+
+    // errors on bad request
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    // adds data bytes to body array
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    // when the upload stream ends
+    request.on('end', () => {
+      // take all the data in the byte array and makes it a string
+      const bodyString = Buffer.concat(body).toString();
+      // parses the string into objects by field name
+      const bodyParams = query.parse(bodyString);
+      // passes to addUser function
+      jsonHandler.addUser(request, res, bodyParams);
+    });
+  } 
+};
+
+const onRequest = (request, response) => {
+  // parse the url using the url module
+  // This will let us grab any section of the URL by name
+  const parsedUrl = url.parse(request.url);
+
+  if (request.method === 'POST') {
+    // handles the post request
+    handlePost(request, response, parsedUrl);
+  } else {
+    // handles the get request
+    handleGet(request, response, parsedUrl);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
